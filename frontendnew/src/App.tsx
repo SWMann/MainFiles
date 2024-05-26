97a47433bc4d80545a16e907f9fe2038297ee132
SWMann <schaeferwmann@gmail.com>
2024-05-26 18:37:31 +0100

    test

--- frontendnew/src/App.tsx
+++ frontendnew/src/App.tsx
@@ -1,21 +1,48 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import ProtectedRoute from "./components/ProtectedRoute.tsx"
+
 import "./App.css";
 import LandingPage from "@/pages/LandingPage.tsx";
-import Introduction from "@/pages/Introduction";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Introduction from "@/pages/Introduction";
 import UnitManagment from "@/pages/UnitManagment";
 // test
 function App() {
+
+
+ // function Logout() {
+   // localStorage.clear()
+   // return <Navigate to="/login" />
+  //}
+  
+  //function RegisterAndLogout() {
+  //  localStorage.clear()
+  //  return <Register />
+  //}
+
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/introduction" element={<Introduction />} />
-          <Route path="/unitmanagment" element={<UnitManagment />} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <UnitManagment />
+            </ProtectedRoute>
+          }
+        />
+        {/* <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<RegisterAndLogout />} /> */}
+        <Route path="*" element={<LandingPage />}></Route>
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
 
 export default App;
+
+
+
